62bf36d7f4007975c28baeb8e93ab74b3eba9b2e
Van <vannv0820@gmail.com>
2021-09-15 14:43:06 +0700

    client init#3

---
+++ PizzaVan/src/app/main/home/home.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const homeRoutes: Routes = [
+  {path: '', redirectTo: 'index', pathMatch: 'full'},
+  {path: 'index', component: HomeComponent}
+]
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(homeRoutes)
+  ]
+})
+export class HomeModule { }
